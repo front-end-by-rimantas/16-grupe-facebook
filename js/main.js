22dc893fd0369755198427c90c9e91062e0e31fb
Rimantas <belauzas@yahoo.com>
2020-07-31 20:44:55 +0300

    Clicking profile picture alt text print to console

--- js/main.js
+++ js/main.js
@@ -14,3 +14,32 @@ function expandPostContent() {
 for ( let i=0; i<moreCount; i++ ) {
     moreElements[i].addEventListener('click', expandPostContent);
 }
+
+// ----------------------------------------
+
+// paspaudus ant profilio nuotraukos, i console parasyti tos nuotraukos alt turini
+
+const photos = document.querySelectorAll('.user-photo > img');
+const photosCount = photos.length;
+
+console.log('Is viso rasta nuotrauku:', photosCount);
+
+function photoClick( event ) {
+    console.log( event );
+
+    const clickedElement = event.target;
+    console.log( clickedElement );
+
+    const altValue = event.target.getAttribute('alt');
+    console.log( altValue );
+}
+
+for ( let i=0; i<photosCount; i=i+2 ) {
+    photos[i].addEventListener('click', photoClick);
+}
+
+
+// function addEventListener( eventType, whatFunctionToCallAfterEvent ) {
+//     let allEventInfo = {}
+//     whatFunctionToCallAfterEvent( allEventInfo )      // photoClick( allEventInfo )
+// }
