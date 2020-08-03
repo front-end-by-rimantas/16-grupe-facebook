616e4a5b98b9a134cd34756a43a71b8188e0c969
Rimantas <belauzas@yahoo.com>
2020-08-03 18:08:28 +0300

    Empty layout fix, no more data from local file

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 "use strict";
-import posts from './data.js';
+// import posts from './data.js';
 import renderFeed from './renderFeed.js';
 
-renderFeed( posts );
+renderFeed(posts);
 
 // EVENT: see more
 const moreElements = document.querySelectorAll('.more');
 const moreCount = moreElements.length;
 
-function expandPostContent( event ) {
+function expandPostContent(event) {
     const parent = event.target.closest('p');
     parent.innerText = parent.dataset.fullText;
 }
 
-for ( let i=0; i<moreCount; i++ ) {
+for (let i = 0; i < moreCount; i++) {
     moreElements[i].addEventListener('click', expandPostContent);
 }
